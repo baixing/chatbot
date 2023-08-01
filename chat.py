472696b02d0b4cebc97e786a40c0f994233a10d2
Jamie Cao <jc2974@cornell.edu>
2023-08-01 17:13:26 +0800

    加注释

--- chat.py
+++ chat.py
@@ -4,7 +4,7 @@ from utils import render_messages, create_chatbot
 
 
 # ------------------------------网页------------------------------
-page_title = "ChatBot"
+page_title = "ChatBot"  # 网页标题
 st.set_page_config(
     page_title=page_title,
     page_icon="random",
@@ -13,33 +13,36 @@ st.set_page_config(
     },
 )
 
-# ------------------------------侧边栏------------------------------
+# ------------------------------配置------------------------------
+# 侧边栏配置
 with st.sidebar:
     st.header("机器人配置")
     system_message = st.text_area(label="角色设定", placeholder="机器人需要扮演什么角色？")
     model = st.selectbox("模型", options=["GPT-3.5", "GPT-4", "Claude 2"])
     temperature = st.slider("随机性", min_value=0.0, max_value=1.0, step=0.01, value=0.0)
-    change_config = st.button(label="更改配置")
+    change_config = st.button(label="确认配置")
     clean_history = st.button(label="清空对话历史")
 
+# 若第一次进入网页或切换了页面，则重置对话历史
 if "current_page" not in st.session_state:
     st.session_state.current_page = page_title
-
 if st.session_state.current_page != page_title or "chatbot" not in st.session_state:
     st.session_state.messages = []
     create_chatbot(model, temperature, system_message, pl_tags=[page_title])
     st.session_state.current_page = page_title
 
+# 清空对话历史并重置ChatBot
 if clean_history:
     st.session_state.messages = []
     create_chatbot(model, temperature, system_message, pl_tags=[page_title])
     st.info("对话历史已清空！", icon="✅")
 
+# 更改ChatBot配置
 if change_config:
     create_chatbot(model, temperature, system_message, pl_tags=[page_title])
     st.info("机器人配置已更改！", icon="✅")
 
-# ------------------------------对话框------------------------------
+# ------------------------------对话------------------------------
 st.title(page_title)  # 渲染标题
 render_messages(st.session_state.messages)  # 渲染对话历史
 if user_message := st.chat_input("你好！"):
